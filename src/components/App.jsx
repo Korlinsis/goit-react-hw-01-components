ba7b324433fcda132d28d902d74f79d13b96546e
Ivan Kiziuk <ivanmnksos@gmail.com>
2023-03-11 23:05:28 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,8 @@ import Profile from './Profile/Profile';
 import user from '../user.json';
 import Statistics from './Statistics/Statistics';
 import data from '../data.json';
-import friends from '../friends.json';
-import transactions from '../transactions.json';
+// import friends from '../friends.json';
+// import transactions from '../transactions.json';
 
 export const App = () => {
   return (
